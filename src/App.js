2342cee851ff812fe8a30082bc4c49d7c9e01e75
shahid-22 <shahidvk1212@gmail.com>
2023-05-10 10:49:25 +0530

    netflix complete

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from "react";
 import Navbar from './components/Navbar/Navbar';
 import Banner from './components/Banner/Banner'
-import Rowpost from "./components/Rowpost/rowpost"
+import Rowpost from "./components/Rowpost/Rowpost"
 import "./App.css"
+import {action,romance,originals,trending,horror} from "./urls"
 function App() {
   return (
       <div className="App">
       < Navbar/>
       < Banner />
-      <Rowpost title="Netflix-Originals" />
-      <Rowpost title="Action" issmall />
-      <Rowpost title="Action" issmall />
+      <Rowpost url={originals} title="Netflix-Originals" />
+      <Rowpost url={trending} title="Trending" issmall />
+      <Rowpost url={action} title="Action" issmall />
+      <Rowpost url={romance} title="Romance" issmall />
+      <Rowpost url={horror} title="Horror" issmall />
       </div>
   )
   }
